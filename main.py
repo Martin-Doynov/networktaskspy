39adb6ac7b7812b54b0d276a619279679158d5d3
Martin-Doynov <martindoynovbg@gmail.com>
2024-04-30 00:11:54 +0300

    Determines if a connection exists between two IP addresses

--- main.py
+++ main.py
@@ -131,4 +131,94 @@ def task1():
     last_address = LastAddress()
     print("Last Address (Default gateway):",last_address)
 
-task1()
+# task1()
+
+
+def task2():
+    """
+    Inputs:
+        Two IP Addresses with their corresponding subnet masks
+
+    Outputs:
+        Determines whether there is a connection between them
+
+    """
+    # If the two IP Addresses' corresponding network addresses are equal, there is a connection.
+    # perhaps the functions from task 1 should have been global... whoops
+
+    ip_address1 = input("IP Address 1: ")
+    sub_mask1 = input("Subnet mask 1: ")
+
+    ip_address2 = input("IP Address 2: ")
+    sub_mask2 = input("Subnet mask 2: ")
+
+    def BinaryIPAddress(ip_address):
+        ip_split_dec=ip_address.strip().split(".")
+        # print(ip_split_dec) # length 4
+
+        ip_split_bin = []
+
+        for a in ip_split_dec:
+            bin_a = bin(int(a))
+            bin_a = bin_a[2:]
+            if len(bin_a) < 8:
+                for a in range(8-len(bin_a)):
+                    bin_a="0"+bin_a
+            # print(bin_a)
+            ip_split_bin.append(bin_a)
+
+
+        return ip_split_bin
+
+    ip_bin1 = BinaryIPAddress(ip_address1)
+    sub_bin1 = BinaryIPAddress(sub_mask1)
+
+    ip_bin2 = BinaryIPAddress(ip_address2)
+    sub_bin2 = BinaryIPAddress(sub_mask2)
+
+    print("IP Binary 1:",ip_bin1)
+    print("Subnet Binary 1:",sub_bin1)
+
+    print("IP Binary 2:",ip_bin2)
+    print("Subnet Binary 2:",sub_bin2)
+
+    def findPrefix(ip_bin):
+        counter=0
+        for a in ip_bin[-1]:
+            if a == "0": counter+=1
+        return 32-counter
+
+    prefix1 = findPrefix(sub_bin1)
+    prefix2 = findPrefix(sub_bin2)
+
+    def NetworkAddress(ip_bin,prefix):
+        lastoctet = ip_bin[-1]
+        host_bytes = 32-prefix
+        # print("Last octet:",lastoctet)
+        newoctet = lastoctet[:8-host_bytes]
+
+        for a in range(host_bytes):
+            newoctet+="0"
+
+        network_address_bin = ip_bin
+        del network_address_bin[-1]
+        network_address_bin.append(newoctet)
+
+        network_address_dec = []
+
+        for a in network_address_bin:
+            decimalint = int(a,2)
+            network_address_dec.append(decimalint)
+
+        return network_address_dec
+
+    network_address1 = NetworkAddress(ip_bin1,prefix1)
+    network_address2 = NetworkAddress(ip_bin2,prefix2)
+
+    print("Network address 1:",network_address1)
+    print("Network address 2:",network_address2)
+
+    if(network_address1==network_address2): print("In the same network!")
+    else: print("Not in the same network...")
+
+task2()
